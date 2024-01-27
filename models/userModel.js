8f480af00c364717f79adb2a3bf65e43e6772db3
Khushboo Malik <mail.khushboomalik@gmail.com>
2024-01-27 13:30:34 +0530

    Added 'other' option in gender field for signup

--- models/userModel.js
+++ models/userModel.js
@@ -18,7 +18,7 @@ const userSchema=new mongoose.Schema({
     gender:{
         type:String,
         required:true,
-        enum:["male","female"],
+        enum:["male","female","other"],
     },
     mobile_number:{
         type:String,
